fa938ae11f8bf9f4a93a4512c34942a196d4cb14
gvarys <johnnyzaev@icloud.com>
2022-06-09 21:24:43 +0300

    d00 ex04 start

---
+++ d00/Assets/ex04/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private KeyCode upKey;
+	[SerializeField] private KeyCode downKey;
+	[SerializeField] private Transform[] collisions;
+	[SerializeField] private float movementSpeed;
+	private const int TopCollision = 0;
+	private const int DownCollision = 1;
+
+	private void Update()
+	{
+		if (Input.GetKey(upKey) && transform.position.y + 0.5f < collisions[TopCollision].position.y)
+			transform.Translate(Vector3.up * (Time.deltaTime * movementSpeed));
+		if (Input.GetKey(downKey) && transform.position.y - 0.5f > collisions[DownCollision].position.y)
+			transform.Translate(Vector3.down * (Time.deltaTime * movementSpeed));
+	}
+}
